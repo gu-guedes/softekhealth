475ba95a80d4e1d6dffe193b839138c3c99b2fe5
Renan Perão <peraorenan@gmail.com>
2025-05-22 21:02:26 -0300

    chore: initialize Android project with Compose, Room, Hilt and charting dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.0"  // Remover esta linha
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
 }
